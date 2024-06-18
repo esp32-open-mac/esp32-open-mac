30423b1e69292d879238621ed2702d465c7e18a9
Frostie314159 <frostie.neuenhausen@gmail.com>
2024-06-18 14:48:00 +0200

    Implement AGC enablement.

--- main/hardware.c
+++ main/hardware.c
@@ -129,6 +129,23 @@ tx_hardware_slot_t tx_slots[TX_SLOT_CNT] = {0};
 
 uint32_t seqnum = 0;
 
+extern uint32_t esp_dport_access_reg_read(uint32_t);
+
+// [[openmac-coverage:implemented]]
+void enabled_wifi_agc_openmac() {
+	write_register(0x3ff5c080, esp_dport_access_reg_read(0x3ff5c080) & ~(0x1));
+	write_register(0x3ff5c030, (esp_dport_access_reg_read(0x3ff5c030) & 0xffffffcf) | 0x10);
+	write_register(0x3ff5c01c, (esp_dport_access_reg_read(0x3ff5c01c) & 0xff00ffff) | 0xc0000);
+	write_register(0x3ff5c038, esp_dport_access_reg_read(0x3ff5c038) & 0x4000000);
+}
+// [[openmac-coverage:implemented]]
+void disable_wifi_agc_openmac() {
+	write_register(0x3ff5c01c, (esp_dport_access_reg_read(0x3ff5c01c) & 0xff00ffff) | 0x7f0000);
+	write_register(0x3ff5c038, esp_dport_access_reg_read(0x3ff5c038) & 0x4000000);
+	write_register(0x3ff5c030, esp_dport_access_reg_read(0x3ff5c030) & 0xffffffcf);
+	write_register(0x3ff5c080, esp_dport_access_reg_read(0x3ff5c080) & 0x1);
+}
+
 void log_dma_item(dma_list_item* item) {
 	ESP_LOGD("dma_item", "cur=%p owner=%d has_data=%d length=%d size=%d packet=%p next=%p", item, item->owner, item->has_data, item->length, item->size, item->packet, item->next);
 }
--- main/hardware.h
+++ main/hardware.h
@@ -17,3 +17,6 @@ extern uint8_t module_mac_addr[6];
 
 #define _MMIO_DWORD(mem_addr) (*(volatile uint32_t *)(mem_addr))
 #define _MMIO_ADDR(mem_addr) ((volatile uint32_t*)(mem_addr))
+
+void enabled_wifi_agc_openmac();
+void disable_wifi_agc_openmac();
